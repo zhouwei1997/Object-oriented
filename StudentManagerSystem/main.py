8df107231b689351ecb83394a6a07e731d7329d4
zhouwei1997 <hengyu19971023@163.com>
2023-02-21 14:07:48 +0800

    面向对象的学员管理系统-程序入口

---
+++ StudentManagerSystem/main.py
@@ -0,0 +1,10 @@
+# -*- coding:utf-8 -*-
+# @author zhouwei
+# @date 2023/2/21 10:46
+# @file main.py
+from StudentManagerSystem.managerSystem import StaudentManager
+
+# 启动学员管理系统
+if __name__ == "__main__":
+    student_manager = StaudentManager()
+    student_manager.run()
